fb4fe8d19342ba4dfa2bb50d0498e49564a96a67
Affani Yusuf <affani14@gmail.com>
2024-11-01 14:15:22 +0700

    Change to dropdownButton2

--- lib/dropdown/main.dart
+++ lib/dropdown/main.dart
@@ -1,67 +1,136 @@
 import 'package:flutter/material.dart';
-import 'package:dropdown_search/dropdown_search.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
-void  main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      title: 'DropdownButton2',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('DropdownButton2 Example'),
+        ),
+        body: MyHomePage(),
+      ),
     );
   }
 }
 
-class Homepage extends StatelessWidget {
-  List<Map<String, dynamic>> dataList = [
-    {
-      "country": "Brazil",
-      "id": 1,
-    },
-    {
-      "country": "Brazil",
-      "id": 2,
-    },
-    {
-      "country": "Brazil",
-      "id": 3,
-    },
-    {
-      "country": "Brazil",
-      "id": 4,
-    },
-    {
-      "country": "Brazil",
-      "id": 5,
-    },
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> items = [
+    'Brazil',
+    'Tunisia',
+    'Canada',
+    'India',
+    'Indonesia',
+    'Amerika',
+    'Rusia',
+    'Inggris',
   ];
+
+  String? selectedValue;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dropdown"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: DropdownSearch<Map<String, dynamic>>(
-          mode : Mode.BOTTOM_SHEET,
-          items: dataList,
-          onChanged: (value) => print(value?["id"] ?? null),
-          selectedItem: {
-            "country" : "Canada",
-            "id": 3,
-          },
-          showClearButton: true,
-          showSearchBox: true,
-          popupItemBuilder: (context, item, isSelected) => ListTile(
-            textColor: Colors.red,
-            title: Text(item["country"].toString()),
+      body: Center(
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton2<String>(
+            isExpanded: true,
+            hint: Text(
+              'Select country',
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).hintColor,
+              ),
+            ),
+            items: items
+                .map((item) => DropdownMenuItem(
+              value: item,
+              child: Text(
+                item,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ))
+                .toList(),
+            value: selectedValue,
+            onChanged: (value) {
+              setState(() {
+                selectedValue = value;
+              });
+            },
+            buttonStyleData: const ButtonStyleData(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              height: 50,
+              width: 200,
+            ),
+            dropdownStyleData: const DropdownStyleData(
+              maxHeight: 200,
+            ),
+            menuItemStyleData: const MenuItemStyleData(
+              height: 50,
+            ),
+            dropdownSearchData: DropdownSearchData(
+              searchController: textEditingController,
+              searchInnerWidgetHeight: 60,
+              searchInnerWidget: Container(
+                height: 60,
+                padding: const EdgeInsets.only(
+                  top: 8,
+                  bottom: 4,
+                  right: 8,
+                  left: 8,
+                ),
+                child: TextFormField(
+                  expands: true,
+                  maxLines: null,
+                  controller: textEditingController,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 8,
+                    ),
+                    hintText: 'Search for country ',
+                    hintStyle: const TextStyle(fontSize: 12),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ),
+              searchMatchFn: (item, searchValue) {
+                return item.value.toString()
+                .toString()
+                    .toLowerCase()
+                    .contains(searchValue.toLowerCase());
+              },
+            ),
+            // Clears the search value when you close the menu
+            onMenuStateChange: (isOpen) {
+              if (!isOpen) {
+                textEditingController.clear();
+              }
+            },
           ),
-          dropdownBuilder: (context, selectedItem) => Text(selectedItem?["country"].toString() ?? "Belum pilih negara"),
         ),
       ),
     );
